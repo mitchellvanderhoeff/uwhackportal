2a7ff9c3840436ba072055fe21ab2bd35f94bf43
Mitchell Vanderhoeff <mitchellvanderhoeff@gmail.com>
2014-03-29 02:52:18 -0400

    Shaping up submit dialog wohoooo

--- app/scripts/SubmitCtrl.js
+++ app/scripts/SubmitCtrl.js
@@ -1,3 +1,23 @@
 /**
  * Created by mitch on 2014-03-29.
  */
+function SubmitCtrl($scope)
+{
+   $scope.name = "";
+   $scope.authors = [];
+
+   $scope.addAuthor = function() {
+      $scope.authors.push({
+         name: ""
+      })
+   };
+
+   $scope.removeAuthorAtIndex = function (index) {
+      if ($scope.authors.length <= 1) {
+         return;
+      }
+      $scope.authors.splice(index, 1);
+   };
+
+   $scope.addAuthor()
+}
